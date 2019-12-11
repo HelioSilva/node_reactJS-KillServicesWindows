d45c6a8bfa58d8be8df014167a7ecc1b67fb7ee2
Helio Silva <suporte.computek@gmail.com>
2019-12-11 12:28:39 -0200

    Add função que realiza a requisicao do status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,57 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import axios from './services/api';
 
-async function requisicao(){
-  const resp = await axios.get('/consulta/00657034000153');
-  console.log(resp);
-  return resp
-}
+function App() {
 
-function  App() {
+  const [data,setData] = useState([]);
 
-  requisicao();
-  
+
+  async function alterarStatus(cnpj,newStatus){
+    const response = await axios.get('/status/'+cnpj+'/'+newStatus);
+    loadClientes();
+    return null ;
+  }
+
+  async function loadClientes(){
+    console.log('Consultando api...');
+    const response = await axios.get('/clientes') ; 
+    setData(response.data);
+    console.log(response.data);
+  }
+
+
+  useEffect(()=>{   
+
+    loadClientes();
+
+  },[data]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Helio da Silva Filho
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+
+        <ul>
+        {
+          data.map(cli => (
+          <li key={cli._id}>
+              <div className="topoItem">
+                      <strong>{cli.razao}</strong>
+              </div>
+              <div className="corpoItem">
+                      <span>CNPJ: {cli.cnpj}</span>
+                      <span>Tel: {cli.telefone}</span>
+              </div>
+              <div className="rodapeItem">
+                      <button>Editar</button>
+                      <button onClick={()=>{alterarStatus(cli.cnpj,!cli.ativo)}} className={(cli.ativo) ? "aprovado" : "bloqueado"} >{(cli.ativo) ? "APROVADO" : "BLOQUEADO"}</button>
+                    
+              </div>
+          </li> ))
+        }
+        </ul>
+
+        }
       </header>
     </div>
   );
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,5 +2,5 @@ import axios from 'axios' ;
 
 
 export default axios.create({
-    baseURL : 'http://heliosilva.online:7000'
+    baseURL : 'http://localhost:7000'
 });
